25a23c3acc1728ebf46cc55d3c2f43098e2aa6d1
Jakub Cieślik <kuba8172@gmail.com>
2026-09-11 14:06:20 +0200

    feat: not found global handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :entity_not_found
+
+  private
+  def entity_not_found
+    render json: { error: "not found" }, status: :not_found
+  end
 end
